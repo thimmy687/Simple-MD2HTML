d1e64329bebe3086cd33a84f56da3354bad5ced0
Ricardo Krause <krr32300@othr.onmicrosoft.com>
2017-04-25 14:52:31 +0200

    change err messages and clean db on startup

--- server.ts
+++ server.ts
@@ -2,26 +2,33 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 const MongoClient = require('mongodb').MongoClient;
-const ps = require(__dirname+ '\\src\\MDReplacer.js');
+const ps = require(__dirname + '\\src\\MDReplacer.js');
 //member
 var db;
 
 var parser = new ps.MDReplacer();
 
 // make public folder accessible to public
-app.use(express.static(__dirname +'/public'));
+app.use(express.static(__dirname + '/public'));
 app.use("/css", express.static(__dirname + '/public/css'));
 app.use("/js", express.static(__dirname + '/public/js'));
 
 //database setup
-MongoClient.connect('mongodb://dbuser:dbpassword@ds115131.mlab.com:15131/mongo-test-db', (err,database)=>{
+MongoClient.connect('mongodb://dbuser:dbpassword@ds115131.mlab.com:15131/mongo-test-db', (err, database) => {
     // start the server
-    if(err)return console.log(err);
+    if (err) return console.log('DB-CONNECTION-ERR: ' + err);
     db = database;
     // set port to listen
-    app.listen(process.env.PORT || 3000, ()=>{
+    app.listen(process.env.PORT || 3000, () => {
         console.log('listening on 3000');
     });
+
+    //clean history on startup
+    db.collection('inputs').drop((err, result) => {
+        if (err) return console.log('WARN:' + err);
+        console.log("INFO: history clean!")
+    });
+
 });
 
 // middleware 
@@ -29,40 +36,36 @@ app.set('view engine', 'ejs');
 
 // body-parser extracts the data from the form element and 
 // add them to the body property in the request object
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 //let server read json data
 app.use(bodyParser.json());
 
 // routing
-app.get('/', (req, res)=>{
-    db.collection('inputs').find().toArray((err, result)=>{
-        if(err) return console.log(err);
-        if(result === null){ result = "test";}
-        res.render('index.ejs', {inputs: result});    
+app.get('/', (req, res) => {
+    db.collection('inputs').find().toArray((err, result) => {
+        if (err) return console.log(err);
+        if (result === null) { result = "test"; }
+        res.render('index.ejs', { inputs: result });
     });
 });
 
-app.post('/inputs', (req, res)=>{
-    var parsedText = parser.replace(req.body.input+'');    
-    var toSave = 
+app.post('/inputs', (req, res) => {
+    var parsedText = parser.replace(req.body.input + '');
+    var toSave =
         {
             name: req.body.name,
             input: req.body.input,
             output: parsedText
         }
     var history;
- 
-    db.collection('inputs').save(toSave, (err,result)=>{
-        if(err)return console.log(err);
-        console.log('saved to database');
-        db.collection('inputs').find().toArray((err,result)=>{
-            if(err) return console.log(err);
+
+    db.collection('inputs').save(toSave, (err, result) => {
+        if (err) return console.log('SAVE-DB-ERR:' + err);
+        console.log('INFO: saved to database');
+        db.collection('inputs').find().toArray((err, result) => {
+            if (err) return console.log('FIND-ON-DB-ERR: ' + err);
             history = result;
-            var check = db.collection('inputs').find();
-            if(check.count() > 20){
-                db.collection('inputs').findOneAndDelete({name: 'input'});
-            }
             res.setHeader('Content-Type', 'application/json');
             res.redirect('/');
         });
@@ -70,26 +73,26 @@ app.post('/inputs', (req, res)=>{
 });
 
 app.put('/inputs', (req, res) => {
-  db.collection('inputs')
-  .findOneAndUpdate({name: 'input'}, {
-    $set: {
-      name: req.body.name,
-      input: req.body.input,
-      output: req.body.output
-    }
-  }, {
-    sort: {_id: -1},
-    upsert: true
-  }, (err, result) => {
-    if (err) return res.send(err)
-    res.send(result)
-  });
+    db.collection('inputs')
+        .findOneAndUpdate({ name: 'input' }, {
+            $set: {
+                name: req.body.name,
+                input: req.body.input,
+                output: req.body.output
+            }
+        }, {
+            sort: { _id: -1 },
+            upsert: true
+        }, (err, result) => {
+            if (err) return res.send(err)
+            res.send(result)
+        });
 });
 
 app.delete('/inputs', (req, res) => {
-        db.collection('inputs').drop((err, result) => {
-            if (err) return res.status(500).send(err);
-            console.log("delete something!")
-            res.status(200).send('Input deleted')
-        });
+    db.collection('inputs').drop((err, result) => {
+        if (err) return res.status(500).send(err);
+        console.log("INFO: history clean!")
+        res.status(200).send("INFO: history clean!")
+    });
 });
